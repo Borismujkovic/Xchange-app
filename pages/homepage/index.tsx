aec8f3db7e6000bba267d7ea257fc6254229985d
Boris Mujkovic <boris.mujkovic@dataart.com>
2023-11-24 12:22:33 +0100

    homepage

---
+++ pages/homepage/index.tsx
@@ -0,0 +1,51 @@
+import Head from "next/head";
+
+import CryptoExchange from "../../src/components/UIDash/CryptoExchange/CryptoExchange";
+import CryptoValue from "../../src/components/UIDash/CryptoValue/CryptoValue";
+import Layout from "../../src/components/layout/Layout";
+import BaseLayout from "../../src/components/sidebar/BaseLayout";
+import { Row, Col } from "antd";
+
+import { useUser } from "@/src/store/user";
+import { useRouter } from "next/router";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+// import logo from "../../src/assets/logo.svg";
+
+export default function Home() {
+  const { user } = useUser();
+  const router = useRouter();
+
+//   useEffect(() => {
+//     if (!user) {
+//       router.push("/");
+//     }
+//   }, [user, router]);
+
+//   if (!user) {
+//     return null;
+//   }
+
+  return (
+    <>
+      <Head>
+        <title>CoineX App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <BaseLayout>
+          <Layout>
+            <Row gutter={40}>
+              <Col span={24}>
+                <CryptoExchange />
+              </Col>
+              <Col span={24}>
+                <CryptoValue />
+              </Col>
+            </Row>
+          </Layout>
+        </BaseLayout>
+      </main>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,89 +9,37 @@ import { Row, Col } from "antd";
 
 import { useUser } from "@/src/store/user";
 import logo from "../src/assets/logo.svg";
+import SignInForm from "@/src/components/auth/SignInForm/SignInForm";
 
 export default function Home() {
   const { user } = useUser();
 
   if (!user) {
     return (
-      <>
-        <Head>
-          <title>CoineX App</title>
-          <link rel="icon" href={logo} />
-        </Head>
-        <main>
-          <BaseLayout>
-            <Layout>
-              <Row gutter={40}>
-                <Col span={23}>
-                  <CryptoExchange />
-                </Col>
-              </Row>
-              <Row gutter={40}>
-                <Col lg={16} md={14} sm={12} xs={23}>
-                  <Row gutter={40}>
-                    <Col span={24}>
-                      <CryptoValue />
-                    </Col>
-                    {/* <Col span={24}>
-                      <TradingActivities />
-                    </Col> */}
-                  </Row>
-                </Col>
-                <Col lg={7} md={9} sm={11} xs={24} >
-                  <SearchCrypto />
-                </Col>
-              </Row>
-              {/* <Row>
-                <Col span={23}>
-                  <TradingActivities />
-                </Col>
-              </Row> */}
-            </Layout>
-          </BaseLayout>
-        </main>
-      </>
+   <SignInForm />
     );
   }
 
-  return (
-    <>
-      <Head>
-        <title>CoineX App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <BaseLayout>
-          <Layout>
-            <Row gutter={40}>
-              <Col span={24}>
-                <CryptoExchange />
-              </Col>
-              <Col span={24}>
-                <CryptoValue />
-              </Col>
-            </Row>
-            <Row>
-              <Col lg={15} md={15} sm={24} offset={1}>
-                {/* <TradeCrypto />
-                <AccountSummary /> */}
-                {/* <TradingActivities /> */}
-              </Col>
-              <Col lg={7} md={7} sm={24} offset={1}>
-                {/* <SearchCrypto />
-                <History /> */}
-              </Col>
-              {/* <QuickTrade /> */}
-            </Row>
-            {/* <Row>
-              <Col span={23} offset={1}>
-                <TradingActivities />
-              </Col>
-            </Row> */}
-          </Layout>
-        </BaseLayout>
-      </main>
-    </>
-  );
+  // return (
+  //   <>
+  //     <Head>
+  //       <title>CoineX App</title>
+  //       <link rel="icon" href="/favicon.ico" />
+  //     </Head>
+  //     <main>
+  //       <BaseLayout>
+  //         <Layout>
+  //           <Row gutter={40}>
+  //             <Col span={24}>
+  //               <CryptoExchange />
+  //             </Col>
+  //             <Col span={24}>
+  //               <CryptoValue />
+  //             </Col>
+  //           </Row>
+  //         </Layout>
+  //       </BaseLayout>
+  //     </main>
+  //   </>
+  // );
 }
